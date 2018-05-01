e2d30ccf1bd876294c7a1c2b5e770763768465e9
George Yuan <georgeyuan@Georges-MacBook-Pro.local>
2018-05-01 15:23:21 -0400

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-console.log(__dirname + '/../public/');
-console.log(publicPath);
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server on port ${port}`);
 });
