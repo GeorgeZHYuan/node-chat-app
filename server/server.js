38615b7219880108af5939846b770b33adfc5c4b
George Yuan <georgeyuan@Georges-MacBook-Pro.local>
2018-05-01 18:32:31 -0400

    added ui using jQuery

--- server/server.js
+++ server/server.js
@@ -16,13 +16,11 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined'));
 
     socket.on('createMessage', (message) => {
         console.log(message);
-        
         io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
@@ -30,8 +28,6 @@ io.on('connection', (socket) => {
         console.log('User disconnected');
     });
 });
-
-
 
 server.listen(port, () => {
     console.log(`server on port ${port}`);
