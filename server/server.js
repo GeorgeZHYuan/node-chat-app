e3baad7d6b0427ca91b13f3340b96cb5cf7ed012
George Yuan <georgeyuan@Georges-MacBook-Pro.local>
2018-05-01 17:38:23 -0400

    incoporated use of generateMessage function

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message.js')
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -15,14 +17,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat'));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined'));
+
     socket.on('createMessage', (message) => {
         console.log(message);
-
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createAt: new Date().getTime()
-        });
+        
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
     socket.on('disconnect', () => {
